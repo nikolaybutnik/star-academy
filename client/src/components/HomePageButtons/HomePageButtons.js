c0f307f69e9f262bbb87ce7bc35f9ec80c17b01f
Louis Yacksmith <louisyscarleton@gmail.com>
2021-01-22 20:24:22 -0500

    css on challenges button

--- client/src/components/HomePageButtons/HomePageButtons.js
+++ client/src/components/HomePageButtons/HomePageButtons.js
@@ -4,8 +4,20 @@ import { Link } from "react-router-dom";
 const HomePageButtons = () => {
   return (
     <Link to="/home">
-      <button className="user-home-btn col-md-12" href="">
-        DAILY QUIZ
+      <button
+        className="user-home-btn col-md-12"
+        href=""
+        style={{
+          margin: "auto",
+          marginTop: "100px",
+          marginBottom: "30px",
+          borderRadius: "0%",
+          backgroundColor: "lightblue",
+          borderWidth: "3px",
+          border: "grey solid thin",
+        }}
+      >
+        <h2 style={{ padding: "5px" }}>DAILY QUIZ</h2>
       </button>
     </Link>
   );
