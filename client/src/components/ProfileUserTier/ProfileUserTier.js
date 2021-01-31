cefe9de6fadd573c0deff44daa0a1ffbac7fdd36
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-01-30 22:32:02 -0500

    Change the displayed profile picture based on user's class.

--- client/src/components/ProfileUserTier/ProfileUserTier.js
+++ client/src/components/ProfileUserTier/ProfileUserTier.js
@@ -1,17 +1,35 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import beginner from '../../Assets/beginnerIcon.png'
+import beginnerIcon from '../../Assets/beginnerIcon.png'
+import buskerIcon from '../../Assets/buskerIcon.png'
+import localTalentIcon from '../../Assets/localtalentIcon.png'
 import energyIcon from '../../Assets/bolt-128.png'
 import { useUser } from '../../utils/UserContext'
 
 const ProfileUserTier = () => {
   // Currently logged in user
   const { user } = useUser()
+
+  let imageSource
+  switch (user.class) {
+    case 'Beginner':
+      imageSource = beginnerIcon
+      break
+    case 'Busker':
+      imageSource = buskerIcon
+      break
+    case 'Local Talent':
+      imageSource = localTalentIcon
+      break
+    default:
+      imageSource = localTalentIcon
+  }
+
   return (
     <div className="col-md-4" style={{ padding: '0px' }}>
       <h1 className="user-level">{user ? user.class : null}</h1>
       <img
-        src={beginner}
+        src={imageSource}
         alt="beginner logo"
         className="user-image-profile col-md-12"
       />
