998417351a9fb5883577502dd6b472e71f1f990b
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-01-31 21:22:31 -0500

    Separating code.

--- client/src/utils/UserContext.js
+++ client/src/utils/UserContext.js
@@ -6,6 +6,7 @@ import { isToday, isYesterday, differenceInMinutes, parseJSON } from 'date-fns'
 import updateUser from '../utils/updateUser'
 import registerLoginEvent from '../utils/registerLoginEvent'
 import dailyResetPersonalGoals from '../utils/dailyResetPersonalGoals'
+import userEnergyCheck from '../utils/userEnergyCheck'
 
 // Store token and user object
 const UserContext = React.createContext()
@@ -47,44 +48,10 @@ function UserProvider(props) {
         setUser(user)
 
         // Check if user's energy needs to be topped up since last time.
-        if (user.energy.value < user.maxEnergy) {
-          const now = new Date()
-          const timestamp = parseJSON(user.energy.timestamp)
-          const difference = differenceInMinutes(now, timestamp)
-          console.log(difference)
-          if (difference >= 1 && difference < 2) {
-            user = {
-              ...user,
-              energy: { value: user.energy.value + 1, timestamp: new Date() },
-            }
-            updateUser(user)
-            setUser(user)
-          } else if (difference >= 2 && difference < 3) {
-            user = {
-              ...user,
-              energy: { value: user.energy.value + 2, timestamp: new Date() },
-            }
-            updateUser(user)
-            setUser(user)
-          } else if (difference >= 3) {
-            user = {
-              ...user,
-              energy: { value: user.energy.value + 3, timestamp: new Date() },
-            }
-            updateUser(user)
-            setUser(user)
-          }
-          if (user.energy.value > user.maxEnergy) {
-            user = {
-              ...user,
-              energy: { value: user.maxEnergy, timestamp: new Date() },
-            }
-            updateUser(user)
-            setUser(user)
-          }
-        }
+        userEnergyCheck(user, updateUser, setUser)
       })
   }
+
   return (
     <UserContext.Provider
       value={{ user, setUser, logout, setToken }}
